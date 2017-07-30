9c9249e69579d9ec0b1580e073419d6d2985d150
ttc007 <truongthanhcong1909@gmail.com>
2017-07-31 00:06:10 +0200

    ok

---
+++ view/ckeditorDemo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <script type="text/javascript" src="ckeditor/ckeditor.js"></script>
+</head>
+<body>
+	<textarea name="editor1" id="editor1" rows="10" cols="80">
+    </textarea>
+    <script>
+        CKEDITOR.replace( 'editor1' );
+    </script>
+</body>
+</html>
